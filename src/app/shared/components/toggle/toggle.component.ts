5b4d8a32de636d83bfe9d5d7b6893388689f020c
jan.bednarek <jan.bednarek@valueadd.pl>
2022-03-14 01:53:24 +0100

    alot refactored and added

---
+++ src/app/shared/components/toggle/toggle.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle',
+  templateUrl: './toggle.component.html',
+  styleUrls: ['./toggle.component.scss'],
+})
+export class ToggleComponent {
+  @Input() label = '';
+  @Input() value = false;
+  @Output() toggled: EventEmitter<boolean> = new EventEmitter();
+
+  handleChange(): void {
+    this.value = !this.value;
+    this.toggled.emit(this.value);
+  }
+}
